bef3b33610ec0e9133c0725e18bd13344a6b11f1
Michael Duncan <msduncan82@gmail.com>
2020-07-31 21:09:09 +0000

    added data extracting & loading interfaces

--- src/data/connect_db.py
+++ src/data/connect_db.py
@@ -1,23 +1,30 @@
 import os
 import dotenv
 from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
 
-def create_engine_to_rds(db_name, echo=True):
-    """Connect to RDS database instance described by environment variables in .env"""
+def create_engine_to_rds(db_name, echo=True, db_params=None):
+    """
+    Connect to RDS database instance described by `db_params` or environment variables or variables in .env
+    """
 
     dotenv.load_dotenv()
-    username = os.getenv("RDS_USER")
-    password = os.getenv("RDS_PASS")
-    endpoint = os.getenv("RDS_ENDPOINT")
-    port = os.getenv("RDS_PORT")
 
-    db_string = f"postgres://{username}:{password}@{endpoint}:{port}/{db_name}"
+    db_params = db_params or {}
+    
+    db_params['drivername'] = db_params.get('drivername', os.getenv('RDS_DRIVER', 'postgres'))
+    db_params['username'] = db_params.get('username', os.getenv('RDS_USER', 'postgres'))
+    db_params['password'] = db_params.get('password', os.getenv('RDS_PASS', 'password'))
+    db_params['host'] = db_params.get('host', os.getenv('RDS_HOST', '0.0.0.0'))
+    db_params['port'] = db_params.get('port', os.getenv('RDS_PORT', 5432))
 
-    engine = create_engine(db_string, executemany_mode="batch", echo=echo)
+    db_uri = f'{URL(**db_params)}/{db_name}'
+
+    engine = create_engine(db_uri, executemany_mode="batch", echo=echo)
 
     return engine
 
 
 if __name__ == "__main__":
 
-    engine = create_engine_to_rds("db_name")
+    engine = create_engine_to_rds("postgres")
