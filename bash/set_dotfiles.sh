8b590fd044dfc8613b6b699755d582e2eb99cf8b
Michael Duncan <msduncan82@gmail.com>
2020-08-03 20:54:39 +0000

    skip cloning bash-git-prompt if it exists

--- bash/set_dotfiles.sh
+++ bash/set_dotfiles.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Setting ~/.bashrc and ~/.bash-git-prompt"
-cat .devcontainer/.bashrc > /root/.bashrc
-git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt
+cat .devcontainer/.bashrc >> /root/.bashrc
+[[ -d "~/.bash-git-prompt" ]] || git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt
